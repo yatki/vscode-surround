06e29ba46acd4cbc55faab9ceee9d0e83c04f780
Mehmet <mehmet@yatki.com>
2018-06-02 14:40:28 +0200

    surround snippets are ready

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,52 @@
-'use strict';
+"use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import {
+  workspace,
+  ExtensionContext,
+  commands,
+  window,
+  QuickPickItem,
+  SnippetString
+} from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
+  let config = workspace.getConfiguration("surround");
+  let snippets = config.get("snippets", []);
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-surround" is now active!');
+  function makeQuickPick() {
+    const quickPicks: QuickPickItem[] = [];
+    if (snippets && snippets.length) {
+      snippets.forEach(snippet => {
+        const { label, description } = snippet;
+        quickPicks.push({
+          label,
+          description
+        });
+      });
+    }
+    return quickPicks;
+  }
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+  function applyQuickPick(item: QuickPickItem) {
+    let activeEditor;
+    if ((activeEditor = window.activeTextEditor)) {
+      // Don't run if no active text editor instance available
+      let snippet = snippets.find(s => item.label === s.label);
+      activeEditor.insertSnippet(new SnippetString(snippet.snippet));
+    }
+  }
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+  let disposable = commands.registerCommand("surround.with", () => {
+    window.showQuickPick(makeQuickPick()).then(item => {
+      applyQuickPick(item);
     });
+  });
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
